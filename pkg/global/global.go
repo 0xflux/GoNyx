92e545e2084a2fb7e4a481091215fa945506a314
Ian G <49762827+the-wandering-photon@users.noreply.github.com>
2023-09-24 21:18:05 +0100

    documentation

--- pkg/global/global.go
+++ pkg/global/global.go
@@ -1,5 +1,10 @@
 package global
 
+/*
+Global constants that will be used through the project, these may be
+configuration settings, hardcoded paths, etc.
+*/
+
 const (
 	ClientPort = 34888 // proxy listen port
 	RelayPort  = 34889 // proxy listen port
